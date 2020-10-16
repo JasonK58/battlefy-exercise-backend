2f3316c0185259f3766bef78a5d31e7b12070ae8
Jason Kolenosky <jasonk58@gmail.com>
2020-10-16 11:43:20 -0700

    Get summoner data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const LeagueJS = require('LeagueJS');
 
 const app = express();
 const port = process.env.PORT;
+const leagueJs = new LeagueJS(process.env.API_TOKEN);
 
 app.get('/', function(req, res) {
 	res.status(200);
@@ -12,6 +14,17 @@ app.get('/', function(req, res) {
 app.get('/test', function(req, res) {
 	res.status(200);
 	res.json({message: 'Connection test was a success!'});
+});
+
+app.get('/summoner', function(req, res) {
+	var summonerName;
+
+	leagueJs.Summoner.gettingByName('Doublelift')
+	.then(summoner => {
+		summonerName = summoner.name;
+		res.status(200);
+		res.json({summonerName: summonerName})
+	})
 });
 
 app.listen(port);
